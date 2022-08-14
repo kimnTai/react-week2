a70d9da94005996ef6dcfc2706fcece47d145591
kimn <a9523023650@gmail.com>
2022-08-14 16:56:15 +0800

    [修改] todoList 製作

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,13 @@ import Items from "./components/items";
 
 export default function App() {
     const [tab, setTab] = useState("全部");
-    const todoState = useState([{ checked: true, todo: "把冰箱發霉的檸檬拿去丟" }]);
-    const createTab = () => {
-        return ["全部", "待完成", "已完成"].map((v) => {
-            return (
-                <li key={v}>
-                    <a className={tab === `${v}` ? "active" : ""} onClick={() => setTab(`${v}`)}>
-                        {v}
-                    </a>
-                </li>
-            );
-        });
-    };
+    const [todo, setTodo] = useState([
+        { checked: false, todo: "把冰箱發霉的檸檬拿去丟" },
+        { checked: true, todo: "打電話叫媽媽匯款給我" },
+    ]);
+
+    const _todo =
+        { 待完成: todo.filter(({ checked }) => !checked), 已完成: todo.filter(({ checked }) => checked) }[tab] || todo;
 
     return (
         <div id="todoListPage" className="bg-half">
@@ -26,14 +21,24 @@ export default function App() {
             </nav>
             <div className="container todoListPage vhContainer">
                 <div className="todoList_Content">
-                    <Input todoState={todoState} />
+                    <Input todoState={[todo, setTodo]} />
                     <div className="todoList_list">
-                        <ul className="todoList_tab">{createTab()}</ul>
+                        <ul className="todoList_tab">
+                            {["全部", "待完成", "已完成"].map((v) => (
+                                <li key={v}>
+                                    <a className={tab === `${v}` ? "active" : ""} onClick={() => setTab(`${v}`)}>
+                                        {v}
+                                    </a>
+                                </li>
+                            ))}
+                        </ul>
                         <div className="todoList_items">
-                            <Items todoState={todoState} />
+                            <Items todoState={[_todo, setTodo]} />
                             <div className="todoList_statistics">
-                                <p> 5 個已完成項目</p>
-                                <a>清除已完成項目</a>
+                                <p> {todo.filter(({ checked }) => checked).length} 個已完成項目</p>
+                                <a onClick={() => setTodo((pre) => pre.filter(({ checked }) => !checked))}>
+                                    清除已完成項目
+                                </a>
                             </div>
                         </div>
                     </div>
--- src/components/items.tsx
+++ src/components/items.tsx
@@ -5,10 +5,20 @@ export default function Items({ todoState: [item, setItem] }: { todoState: ITodo
                 return (
                     <li key={i}>
                         <label className="todoList_label">
-                            <input className="todoList_input" type="checkbox" checked={checked} onChange={() => {}} />
+                            <input
+                                className="todoList_input"
+                                type="checkbox"
+                                checked={checked}
+                                onChange={() => {
+                                    setItem((pre) => {
+                                        pre[i].checked = !checked;
+                                        return [...pre];
+                                    });
+                                }}
+                            />
                             <span>{todo}</span>
                         </label>
-                        <a onClick={() => console.log(123)}>X</a>
+                        <a onClick={() => setItem((pre) => pre.filter((_, _i) => _i !== i))}>刪除</a>
                     </li>
                 );
             })}
